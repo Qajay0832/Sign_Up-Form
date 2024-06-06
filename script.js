b2d09a3c13668bc83029fbd8ce93b09f3d953db5
Qajay0832 <qajay0832@gmail.com>
2024-06-06 18:22:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let error1= document.getElementById("error1");
+let error2= document.getElementById("error2");
+let submitPara= document.getElementById("submitPara");
+let submit= document.getElementById("submit");
+
+let email=document.getElementById("email");
+let password=document.getElementById("password");
+
+const emailchange=()=>{
+    if(!email.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+        error1.style.display="block";
+    }
+}
+const passwordChange=()=>{
+    if( !password.value.match(/^.{8,}$/)){
+        error2.style.display="block";
+    }
+}
+const onSubmit=(event)=>{
+    event.preventDefault();
+    if(email.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) && password.value.match(/^.{8,}$/)){
+        error1.style.display="none";
+        error2.style.display="none";
+        submitPara.style.display="block";
+    }
+    else{
+        error1.style.display="block";
+        error2.style.display="block";
+        submitPara.style.display="none";
+    }
+
+}
+submit.addEventListener("click",onSubmit)
